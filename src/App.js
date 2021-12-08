2118b54ebbba13cfb34e6c461ba57dd5760717a1
Anders Killander <killander.anders@gmail.com>
2021-12-08 11:16:48 +0100

    🚀first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+import { signup, useAuth, logout, login } from "./firebase"
+
 import './App.css';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const currentUser = useAuth();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleSignup() {
+    setLoading(true)
+    try {
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch (errors) {
+      alert(errors.message)
+    }
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    setLoading(false)
+  }
+
+   const handleLogout = async () => {
+    setLoading(true)
+    try {
+      await logout()
+    } catch (errors) {
+      alert(errors.message)
+    }
+    setLoading(false)
+  }
+
+   const handleLogin = async () => {
+    setLoading(true)
+    try {
+      await login(emailRef.current.value, passwordRef.current.value)
+    } catch (errors) {
+      alert(errors.message)
+    }
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    setLoading(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Register User</h3>
+        <input disabled={currentUser} placeholder={currentUser ? "Success" : "Email..."} ref={emailRef}/>
+        <input disabled={currentUser} type="password" placeholder={currentUser ? "Success" : "Password..."} ref={passwordRef} />
+
+      <div>Signed in user: {currentUser?.email} </div>
+      <div className="buttonContainer">
+        <button disabled={ loading || currentUser } onClick={handleSignup}>Create User</button>
+        <button disabled={ loading || !currentUser } onClick={handleLogout}>Log Out</button>
+        <button disabled={ loading || currentUser } onClick={handleLogin}>Sign In</button>
+        </div>
+
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/firebase.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDrPme2pSYDtC-FcjaWh7Y9ZXT8Wl6rWVQ",
+    authDomain: "authentication-app-fb3de.firebaseapp.com",
+    projectId: "authentication-app-fb3de",
+    storageBucket: "authentication-app-fb3de.appspot.com",
+    messagingSenderId: "357894488286",
+    appId: "1:357894488286:web:48b26035edaf6da82dc24f",
+    measurementId: "G-18V1086ZLZ"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export function signup(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+export function logout() {
+  return signOut(auth)
+}
+
+// Custom Hook
+export function useAuth() {
+  const [ currentUser, setCurrentUser ] = useState();
+  
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, user => setCurrentUser(user) )
+    return unsub;
+  }, [])
+  
+  return currentUser;
+}
